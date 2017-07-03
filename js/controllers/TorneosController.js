fbe79428230387d1b1606ec34efd6a1f8638c08e
martinoki <martinoki@hotmail.com>
2017-07-03 15:39:07 -0300

    Mapas agregados, pantalla de torneos agregada, separacion de jugadores por equipo

---
+++ js/controllers/TorneosController.js
@@ -0,0 +1,5 @@
+torneoFutbol.controller('TorneosCtrl', function ($scope, $rootScope, $location, $modal, $cookieStore, $filter, $translate, DataService, DTOptionsBuilder, DTColumnDefBuilder, DTColumnBuilder) {
+
+
+
+});
