6f18dad6a1c25459af25a5eb1ab190143c694dcf
martinoki <martinoki@hotmail.com>
2017-07-20 11:30:27 -0300

    Gestion de Torneos
    alta de torneos, agregar equipos y jugadores al torneo

--- js/controllers/TournamentsManagementController.js
+++ js/controllers/TournamentsManagementController.js
@@ -38,9 +38,72 @@ torneoFutbol.controller('TournamentsManagementCtrl', function ($scope, $rootScop
         }
     };
 
+    $scope.showFormatName = function(tournamentFormat){
+    	if(tournamentFormat){
+    		switch(tournamentFormat){
+    			case 1:
+    				return 'Fútbol 5';
+
+    			case 2:
+    				return 'Fútbol 6';
+
+    			case 3:
+    				return 'Fútbol 7';
+
+    			case 4:
+    				return 'Fútbol 8';
+
+    			case 5:
+    				return 'Fútbol 9';
+
+    			case 6:
+    				return 'Fútbol 11';
+    		}
+    	}
+    };
+
+    $scope.showClasificationFormatName = function(clasificationFormat){
+    	if(clasificationFormat){
+    		switch(clasificationFormat){
+    			case 1:
+    				return 'Liga (Zona Clasificacion + Zona Definicion)';
+
+    			case 2:
+    				return 'Liga (Zona Clasificacion + PlayOff)';
+
+    			case 3:
+    				return 'Liga';
+
+    			case 4:
+    				return 'Play Off';
+    		}
+    	}
+    }
+
     $scope.newTournament = function(){
     	$scope.editTournament();
     }
+
+    $scope.openRules = function(tournament){
+		var modalInstance = $modal.open ({
+
+			templateUrl: 'rules.html',
+			controller: RulesCtrl,
+			size: 'lg',
+			backdrop: 'static',
+			resolve: {
+				tournament : function(){
+					return tournament;
+				}
+	        }
+      	});
+
+	    modalInstance.result.then(function () {
+      		
+        },function(){
+
+        });
+	}
 
     $scope.editTournament = function(tournament){
 		var modalInstance = $modal.open ({
@@ -69,13 +132,17 @@ var ManageTournamentCtrl = function ($scope, $window, $filter, DataService, $mod
 	
 	$scope.saving = false;
 	$scope.errorMsg = null;
-	$scope.tournament = tournament
+	$scope.selectedTeam = null;
+	$scope.tournament = tournament;
 	var method = 'POST';
 	var url = 'v1/tournaments/'
 	$scope.selection = [];
 	$scope.teamSelection = [];
 	$scope.playerSelection = [];
 	$scope.players = [];
+	$scope.playerTeamsSelection = {
+		"values" : {}
+	}
 
 	$scope.getHeadquarters = function(){
 		DataService.getHeadquarters(function(response){
@@ -98,6 +165,7 @@ var ManageTournamentCtrl = function ($scope, $window, $filter, DataService, $mod
 
 	$scope.showPlayers = function(team){
 		$scope.players = team.PlayersList;
+		$scope.selectedTeam = team;
 	}
 
 	$scope.toggleSelection = function toggleSelection(headquarterId) {
@@ -112,25 +180,38 @@ var ManageTournamentCtrl = function ($scope, $window, $filter, DataService, $mod
 	};
 
 	$scope.toggleTeamSelection = function toggleTeamSelection(teamId) {
-		var idx = $scope.teamSelection.indexOf(teamId);
+		// var idx = $scope.teamSelection.indexOf(teamId);
+		var idx = $scope.playerTeamsSelection.values.hasOwnProperty(teamId);
+
+		// var players = {
+		// 	"values" : {}
+		// };
+
+		// for( var idx in $scope.selection){
+		// 	players.values[$scope.selection[idx].Id] = Eval;
+		// }
 	 
 	    // si está seleccionado
-	    if (idx > -1) {
-	       $scope.teamSelection.splice(idx, 1);
+	    if (idx) {
+	//        	$scope.teamSelection.splice(idx, 1);
+			// $scope.playerTeamsSelection.values.splice(teamId, 1)
+			delete $scope.playerTeamsSelection.values[teamId];
 	    } else {
-	       $scope.teamSelection.push(teamId);
+			$scope.playerTeamsSelection.values[teamId] = [];
+	//        	$scope.teamSelection.push(teamId);
 	    }
 	};
 
 	$scope.togglePlayerSelection = function togglePlayerSelection(playerId) {
-		var idx = $scope.playerSelection.indexOf(playerId);
+		var idx = $scope.playerTeamsSelection.values[$scope.selectedTeam.Id].indexOf(playerId);
 	 
 	    // si está seleccionado
 	    if (idx > -1) {
-	       $scope.playerSelection.splice(idx, 1);
+	       $scope.playerTeamsSelection.values[$scope.selectedTeam.Id].splice(idx, 1);
 	    } else {
-	       $scope.playerSelection.push(playerId);
+	       $scope.playerTeamsSelection.values[$scope.selectedTeam.Id].push(playerId)
 	    }
+		
 	};
 
 
@@ -140,29 +221,52 @@ var ManageTournamentCtrl = function ($scope, $window, $filter, DataService, $mod
 		$scope.clasificationFormat = tournament.ClasificationFormat;
 		$scope.rules = tournament.Rules;
 		$scope.prices = tournament.Prices;
-		$scope.headquarters = tournament.Headquarters;
-		$scope.dates = tournament.Dates
+		
+		if(tournament.HeadquartersList.length != 0){
+			
+			//Recorro la lista de sedes para guardarlas en las seleccionadas
+			for (var i = 0; i < tournament.HeadquartersList.length; i++) {
+				$scope.selection.push(tournament.HeadquartersList[i].Id);
+			}
+		}
+
+		if(tournament.TeamPlayersList.length != 0){
+			var teams = tournament.TeamPlayersList;
+
+			//Recorro la lista de equipos y guardo los ID de los equipos
+			for (var i = 0; i < teams.length; i++) {
+				//Los guardo en el array que uso para
+				$scope.playerTeamsSelection.values[teams[i].Id] = []
+				for (var j = 0; j < teams[i].PlayersList.length; j++) {
+					var players = teams[i].PlayersList;
+					$scope.playerTeamsSelection.values[teams[i].Id].push(players[j].Id)
+				}
+			}
+			// $scope.playerTeamsSelection.values[teamId] = []
+		}
+
+		$scope.dates = tournament.Dates;
 	}
 	
 	$scope.manageTournament = function(){
 
 		if($scope.name == '' || $scope.name == undefined){
-			$scope.errorMsg = 'Por favor, ingrese el nombre del torneo.'
+			$scope.errorMsg = 'Por favor, ingrese el nombre del torneo.';
 			return;
 		}
 
 		if($scope.tournamentFormat == undefined){
-			$scope.errorMsg = 'Por favor, seleccione el formato del torneo.'
+			$scope.errorMsg = 'Por favor, seleccione el formato del torneo.';
 			return;
 		}
 
 		if($scope.clasificationFormat == undefined){
-			$scope.errorMsg = 'Por favor, seleccione el formato de clasificación del torneo.'
+			$scope.errorMsg = 'Por favor, seleccione el formato de clasificación del torneo.';
 			return;
 		}
 
 		if($scope.rules == '' || $scope.rules == undefined){
-			$scope.errorMsg = 'Por favor, ingrese las reglas del torneo.'
+			$scope.errorMsg = 'Por favor, ingrese las reglamento del torneo.'
 			return;
 		}
 
@@ -182,6 +286,14 @@ var ManageTournamentCtrl = function ($scope, $window, $filter, DataService, $mod
 		}
 		var date = [];
 		date.push($scope.dates)
+
+		var teamsAndPlayers = {};
+		for (var i = 0; i < $scope.playerTeamsSelection.values.length; i++) {
+			teams[i]
+		}
+		
+		
+
 		var data = {
 			'Name' : $scope.name,
 			'TournamentFormat' : $scope.tournamentFormat,
@@ -189,8 +301,11 @@ var ManageTournamentCtrl = function ($scope, $window, $filter, DataService, $mod
 			'Rules' : $scope.rules,
 			'Prices' : $scope.prices,
 			'Headquarters' : $scope.selection,
-			'Dates' : date
+			'Dates' : date,
+			'TeamsPlayers' : $scope.playerTeamsSelection.values
 		}
+
+
 
 		DataService.manageTournament(method, url, data, function(response){
 			$modalInstance.close();
@@ -198,6 +313,20 @@ var ManageTournamentCtrl = function ($scope, $window, $filter, DataService, $mod
 			$scope.errorMsg = response.Message;
 		});
 	}
+
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
+    };
+
+};
+
+var RulesCtrl = function ($scope, $window, $filter, DataService, $modalInstance, $translate, tournament) {
+	
+	$scope.saving = false;
+	$scope.errorMsg = null;
+	$scope.selectedTeam = null;
+	$scope.tournament = tournament;
+	
 
     $scope.cancel = function () {
         $modalInstance.dismiss('cancel');
